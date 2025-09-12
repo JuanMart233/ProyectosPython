eb89ffbcb70f080fdd861ae581f64509375d7d53
JuanMart233 <23308060610229@cetis61.edu.mx>
2025-09-12 09:40:46 -0600

    ProyectosPython

--- adivina.py
+++ adivina.py
@@ -1,10 +1,10 @@
 X=50
-mipapa=(input("Adivina el valor de X, teclea un numero y ve acercandote al valor "))
-if mipapa <= X:
+Y=(input("Adivina el valor de X, teclea un numero y ve acercandote al valor "))
+if Y <= X:
     print("tu numero es inferior a X")
 else:
-    if mipapa >= X:
+    if Y >= X:
         print("tu numero es mayor a X")
     else:
-        if mipapa == 0:
+        if Y == 0:
             print("tu numero es igual a X")
